0e55597115833c33d02dfe0327a304e24b813aff
abizjak <abizjak@users.noreply.github.com>
2022-12-08 22:46:01 +0000

    Deploying to gh-pages from @ Concordium/concordium-rust-sdk@37783581f46a07d662dbde713f404d7a5039f4ac 🚀

--- implementors/concordium_rust_sdk/types/transactions/trait.TransactionSigner.js
+++ implementors/concordium_rust_sdk/types/transactions/trait.TransactionSigner.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["concordium_rust_sdk"] = [];
+implementors["concordium_rust_sdk"] = [{"text":"impl <a class=\"trait\" href=\"concordium_rust_sdk/types/transactions/trait.TransactionSigner.html\" title=\"trait concordium_rust_sdk::types::transactions::TransactionSigner\">TransactionSigner</a> for <a class=\"struct\" href=\"concordium_rust_sdk/types/struct.WalletAccount.html\" title=\"struct concordium_rust_sdk::types::WalletAccount\">WalletAccount</a>","synthetic":false,"types":["concordium_rust_sdk::types::WalletAccount"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
